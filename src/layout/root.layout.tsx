e9aea67cb943b83c6e9aefaf4eba0664ccfc860f
Your Name <dulneth.bernard@gmail.com>
2024-01-28 16:45:41 +0530

    first commit(Nutrimate frnt_end  starting templat e)

---
+++ src/layout/root.layout.tsx
@@ -0,0 +1,9 @@
+function RootLayout() {
+    return (  
+        <main className="container">
+            {/* inside this main tag is where all the compoenents are going to go in our project */}
+        </main>
+    );
+}
+
+export default RootLayout;
