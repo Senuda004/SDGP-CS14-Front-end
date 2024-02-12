6d3bcc1234fe8076d49c5dd6606f13a9557bdab6
Senuda004 <senudamusic120@gmail.com>
2024-02-12 20:21:01 +0530

    Added styles to sign-up comp

--- signup-login/src/components/Signup.js
+++ signup-login/src/components/Signup.js
@@ -24,36 +24,47 @@ const Signup = () => {
 
   return (
     <>
-      <div className="p-4 box">
-        <h2 className="mb-3">Nutrimate Signup</h2>
-        {error && <Alert variant="danger">{error}</Alert>}
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control
-              type="email"
-              placeholder="Email address"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
+    <div className="p-10 flex items-center justify-center gap-5 bg-stone-50 rounded-[30px] custom-shadow">
+      {/* form */}
+      <div className="w-1/2">
+        <div className="p-4">
+          <h2 className="mb-3 font-bold text-3xl opacity-70 text-center relative bottom-10">Nutrimate Signup</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                type="email"
+                placeholder="Email address"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
 
-          <div className="d-grid gap-2">
-            <Button variant="primary" type="Submit">
-              Sign up
-            </Button>
-          </div>
-        </Form>
+            <div className="d-grid h-10 p-4 bg-amber-400 rounded-lg justify-center items-center gap-[5px] mb-4 overflow-hidden hover:transform hover:scale-105 transition-transform duration-300">
+              <Button type="Submit" className="border-none relative bottom-4 font-white font-semibold active:bg-amber-400 focus:bg-amber-400 custom-button-hover">
+                Sign up
+              </Button>
+            </div>
+          </Form>
+        </div>
+        <div className="text-center rounded-lg">
+          Already have an account? <Link to="/" className="text-amber-400">Log In</Link>
+        </div>
       </div>
-      <div className="p-4 box mt-3 text-center">
-        Already have an account? <Link to="/">Log In</Link>
+
+      {/* image */}
+      <div className="w-1/2">
+          <img src="https://i.ibb.co/vmDdxRr/Login-Page-PNG.png" alt="" />
       </div>
+
+    </div>
     </>
   );
 };
