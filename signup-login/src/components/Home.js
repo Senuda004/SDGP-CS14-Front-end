8993a474f397b448835dd6916e2560ba8e2ef4f9
Senuda004 <senudamusic120@gmail.com>
2024-02-15 19:41:37 +0530

    Home Update

--- signup-login/src/components/Home.js
+++ signup-login/src/components/Home.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
-import { useUserAuth } from '../context/UserAuthContext';
 
 const Home = () => {
-    const { user, logOut } = useUserAuth();
-    console.log(user);
-
-    const handleLogOut = async () => {
-        try {
-            await logOut();
-        } catch (err) {
-            console.log(err.message)
-        }
-    };
 
     return (
         <>
-            <div className="p-4 box mt-3 text-center">Hello Welcome<br />{user && user.email}</div>
-            <div className="d-grid gap-2">
-                <Button variant="primary" onClick={handleLogOut}>Log out</Button>
+            <div>
+                <div className={'w-72 h-screen '}>Sidebar</div>
             </div>
         </>
     );
