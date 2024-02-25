0017108aad562441531feddc197aff2c8c923e0d
Senuda004 <senudamusic120@gmail.com>
2024-02-25 15:44:17 +0530

    Made the home page responsive

--- signup-login/src/components/AiChatbot.js
+++ signup-login/src/components/AiChatbot.js
@@ -3,11 +3,7 @@ import React from 'react';
 const AiChatbot = () => {
   return (
     <div>
-      <iframe
-        src="https://dulnethbuilds-nutrimate-ai-chatbot.hf.space"
-        frameborder="0"
-        width="100%"
-        height="635"></iframe>
+      
     </div>
   );
 };
