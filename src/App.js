6e5b43ec715d2461db5224e09a6f6cc184851046
Induranga Ekanayake <ekanayakaudantha@gmail.com>
2024-03-08 22:54:52 +0530

    Blog template added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Terms from './components/pages/Terms.jsx';
-import PrivacyPolicy from './components/pages/PrivacyPolicy';
-import QAPage from './components/pages/QAPage';
-
+import Blog from './components/pages/Blog.jsx';
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/q-and-a" element={<QAPage />} />
-        {/* Other routes */}
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </Router>
     
