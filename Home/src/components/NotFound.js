e2a46fec93db1e282c8220139d4fee598ebf3262
Senuda Perera <118674433+Senuda004@users.noreply.github.com>
2024-03-10 18:10:26 +0530

    Add files via upload

---
+++ Home/src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-amber-400">
+      <div className="text-white text-4xl font-bold">
+        404 Not Found
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
