13fd6df1c6fa631f8c6c0e63705aa689d1737ece
Dulneth-Bernard <dulneth.bernard@gmail.com>
2024-03-11 13:59:26 +0530

    My message

--- Home/src/components/ProtectedRoute.js
+++ Home/src/components/ProtectedRoute.js
@@ -1,16 +1,13 @@
-// ProtectedRoute.js
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useUserAuth } from '../context/UserAuthContext';
 
-const ProtectedRoute = ({ component: Component, ...props }) => {
-  let { user } = useUserAuth();
-
-  if (!user) {
-    return <Navigate to="/" />;
-  }
-
-  return <Component {...props} />;
-};
+const ProtectedRoute = ({ children }) => {
+    let { user } = useUserAuth();
+    if (!user) {
+        return <Navigate to="/" />
+    }
+    return children;
+}
 
 export default ProtectedRoute;
