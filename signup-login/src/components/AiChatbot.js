5b52b99c9192aca1388326c1fe084f8deee95702
Your Name <dulneth.bernard@gmail.com>
2024-03-12 08:50:47 +0530

    Hnadled Es-lint errors and other conflicts arrised by Es-lint

--- signup-login/src/components/AiChatbot.js
+++ signup-login/src/components/AiChatbot.js
@@ -6,6 +6,7 @@ const AiChatbot = () => {
       <h1>Chat Bot</h1>
       {/* Integration of Ai chatbot LLM published on Hugging face  */}
       <iframe
+        title='chatbot feature'
         src="https://dulnethbuilds-nutrimate-ai-chatbot.hf.space"
         width="100%"
         height="635"></iframe>
