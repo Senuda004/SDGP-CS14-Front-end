90c916bd0d2a1e9d0e9bcf72a2cd92e9b8fe8029
Your Name <dulneth.bernard@gmail.com>
2024-03-12 08:48:45 +0530

    Integration of Ai chatbot for current status of project

--- signup-login/src/components/AiChatbot.js
+++ signup-login/src/components/AiChatbot.js
@@ -4,6 +4,11 @@ const AiChatbot = () => {
   return (
     <div>
       <h1>Chat Bot</h1>
+      {/* Integration of Ai chatbot LLM published on Hugging face  */}
+      <iframe
+        src="https://dulnethbuilds-nutrimate-ai-chatbot.hf.space"
+        width="100%"
+        height="635"></iframe>
     </div>
   );
 };
