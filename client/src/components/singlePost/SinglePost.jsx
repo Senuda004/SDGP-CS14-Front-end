48fee674423973c273b5840cd05ab0eff70f2bf1
Movindu Jayathilake <147236691+MovinduJay@users.noreply.github.com>
2024-03-23 10:31:51 +0530

    User can abe able to delete a post now

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -9,12 +9,19 @@ export default function SinglePost() {
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({})
   const PF="http://localhost:5000/uploads/"
+  const [updateMode, setUpdateMode] = useState(false)
+  const [title,setTitle] = useState("")
+  const [description,setDesc] = useState("")
+
 
   useEffect(()=>{
-const getPost= async ()=>{
-  const res =  await axios.get("/posts/" + path);
-  setPost(res.data)
-  };
+    const getPost= async ()=>{
+      const res =  await axios.get("/posts/" + path);
+      setPost(res.data)
+      setTitle(res.data.title)
+      setDesc(res.data.description)
+  
+    };
   getPost()
   
   }, [path]);
@@ -28,24 +35,47 @@ const getPost= async ()=>{
     }
   };
 
-
-
+  const handleUpdate = async () => {
+    try {
+      const updatedPost = {
+        title: title,
+        description: description,
+      };
+      await axios.patch(`/posts/${path}`, updatedPost);
+      setUpdateMode(false); // Exit update mode after successful update
+      window.location.reload(); // Reload the page
+    } catch (error) {
+      console.error("Error updating post:", error);
+    }
+  };  
+  
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
         {post.img && (
         <img className="singlePostImg" src={PF +post.img} alt=""/>
-)}
-        <h1 className="singlePostTitle">
-          {post.title}
-          <div className="singlePostEdit">
-            <i className="singlePostIcon far fa-edit"></i>
-            <i
+        )}
+        {updateMode ? (
+          <input 
+          type ="text" 
+          value={title} 
+          className="singlePostTitleInput" 
+          autoFocus
+          onChange={(e) => setTitle(e.target.value)}
+          />
+          ):(
+             <h1 className="singlePostTitle">
+              {post.title}
+              <div className="singlePostEdit">
+              <i className="singlePostIcon far fa-edit" onClick={()=>setUpdateMode(true)}></i>
+              <i
                   className="singlePostIcon far fa-trash-alt"
                   onClick={handleDelete}
                 ></i>
           </div>
         </h1>
+         )
+        }
         <div className="singlePostInfo">
           <span>
             Author:
@@ -59,10 +89,14 @@ const getPost= async ()=>{
           <span>{new Date(post.createdAt).toDateString()}</span>
           </div>
         </div>
-        <p className="singlePostDesc">  
-          {post.description}
-          
-        </p>
+
+        {updateMode ? (
+        <textarea className="singlePostDesc" value={description} onChange={(e) => setDesc(e.target.value)}/>
+        ):(
+        <p className="singlePostDesc"> {post.description}</p>
+          )}
+
+          <button className="singlePostButton" onClick={handleUpdate}>Update</button>
       </div>
     </div>
   );
